b693083696d8aa2a64f52594311afcf36d063c30
Hai Qian <Hai.Qian@emc.com>
2013-03-26 00:50:25 -0700

    Give better names to the classes
    Start implementing lm

--- rmadlib/R/method-as_db_data_frame.R
+++ rmadlib/R/method-as_db_data_frame.R
@@ -1,15 +1,21 @@
 
 ## to be finished
 
+## ------------------------------------------------------------------------
+## Convert other R objects to data.frame.db
+## One can choose to create db.obj or query.obj
+## The latter one can be created from a string
+
+
 ## generic function
 ## @param db.obj - new relation in database (table or view)
 ## @param r.obj - R object which contains data for creating database object
-setGeneric("as.data.frame.indb",
-           function (db.obj, r.obj, ...) standardGeneric("as.data.frame.indb"),
+setGeneric("as.data.frame.db",
+           function (db.obj, r.obj, ...) standardGeneric("as.data.frame.db"),
            signature = c("db.obj", "r.obj"))
 
 ## create table in db from data.frame r.obj 
-setMethod("as.data.frame.indb",
+setMethod("as.data.frame.db",
           signature(db.obj = "character", r.obj = "data.frame"),
           function (db.obj, r.obj, row.names = TRUE, 
                     overwrite = FALSE, append = FALSE,
